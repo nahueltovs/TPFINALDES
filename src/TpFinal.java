43837c8cab0d59beed2b2e4c05946a58d5a3b7df
nahueltovs <nahuelirazabal@gmail.com>
2024-11-27 22:38:23 -0300

    dsa

--- src/TpFinal.java
+++ src/TpFinal.java
@@ -2,7 +2,7 @@ import TDA.Avion;
 import TDA.Ruta;
 import TDA.Vuelo;
 
-public class App {
+public class TpFinal {
     public static void main(String[] args) throws Exception {
         Avion[] aviones = new Avion[50];
         Ruta[] rutas = new Ruta[50];
